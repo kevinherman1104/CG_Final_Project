7a8c2fc83f9542c0bd3dfe60e3a98d0e920754b7
jeremyponto <56902787+jeremyponto@users.noreply.github.com>
2022-01-14 01:48:45 +0800

    Simplified the environment, added cut scenes, custom animations, proximity SFX, and pathfinding

--- main/game.js
+++ main/game.js
@@ -27,6 +27,8 @@ class Game{
 		this.debug = false;
 		this.debugPhysics = false;
 		this.cameraFade = 0.05;
+        this.mute = false;
+        this.collect = [];
 		
 		this.messages = { 
 			text:[ 
@@ -47,14 +49,21 @@ class Game{
 		
 		const sfxExt = SFX.supportsAudioType('mp3') ? 'mp3' : 'ogg';
 		const game = this;
-		this.anims = ["gather-objects", "look-around", "push-button", "run"];
+		this.anims = ["ascend-stairs", "gather-objects", "look-around", "push-button", "run"];
+		this.tweens = [];
 		
 		this.assetsPath = '../assets/';
 		
 		const options = {
 			assets:[
                 `${this.assetsPath}sfx/gliss.${sfxExt}`,
-				`${this.assetsPath}fbx/environment.fbx`
+				`${this.assetsPath}sfx/factory.${sfxExt}`,
+				`${this.assetsPath}sfx/button.${sfxExt}`,
+				`${this.assetsPath}sfx/door.${sfxExt}`,
+                `${this.assetsPath}sfx/fan.${sfxExt}`,
+				`${this.assetsPath}fbx/environment.fbx`,
+				`${this.assetsPath}fbx/girl-walk.fbx`,
+                `${this.assetsPath}fbx/usb.fbx`,
 			],
 			oncomplete: function(){
 				game.init();
@@ -68,12 +77,93 @@ class Game{
 		
 		document.getElementById("camera-btn").onclick = function(){ game.switchCamera(); };
 		document.getElementById("briefcase-btn").onclick = function(){ game.toggleBriefcase(); };
+		document.getElementById("action-btn").onclick = function(){ game.contextAction(); };
+        document.getElementById("sfx-btn").onclick = function(){ game.toggleSound(); };
+		
+		this.actionBtn = document.getElementById("action-btn");
 		
 		this.clock = new THREE.Clock();
 
 		//this.init();
 		//this.animate();
 		const preloader = new Preloader(options);
+	}
+	
+    toggleBriefcase(){
+        const briefcase = document.getElementById("briefcase");
+        const open = (briefcase.style.opacity > 0);
+        
+        if (open){
+            briefcase.style.opacity = "0";
+        }else{
+            briefcase.style.opacity = "1";
+        }
+    }
+    
+    toggleSound(){
+        this.mute = !this.mute;
+        const btn = document.getElementById('sfx-btn');
+        
+        if (this.mute){
+            for(let prop in this.sfx){
+                let sfx = this.sfx[prop];
+                if (sfx instanceof SFX) sfx.stop();
+            }
+            btn.innerHTML = '<i class="fas fa-volume-off"></i>';
+        }else{
+            this.sfx.factory.play
+            this.sfx.fan.play();
+            btn.innerHTML = '<i class="fas fa-volume-up"></i>';
+        }
+    }
+    
+	contextAction(){
+		console.log('contextAction called ' + JSON.stringify(this.onAction));
+		if (this.onAction !== undefined){
+			if (this.onAction.action!=undefined){
+				this.action = this.onAction.action;
+			}
+		}
+		
+		const game = this;
+		
+		if (this.onAction.mode !== undefined){
+			switch(this.onAction.mode){
+				case 'open-doors':
+					this.sfx.door.play();
+					this.sfx.button.play();
+					const door = this.doors[this.onAction.index];
+					const left = door.doors[0];
+					const right = door.doors[1];
+					this.cameraTarget = { position:left.position.clone(), target:left.position.clone() };
+					this.cameraTarget.position.y += 150;
+					this.cameraTarget.position.x -= 950;
+					//target, channel, endValue, duration, oncomplete, easing="inOutQuad"){
+					this.tweens.push( new Tween(left.position, "z", left.position.z - 240, 2, function(){
+						game.tweens.splice(game.tweens.indexOf(this), 1);
+					}));
+					this.tweens.push( new Tween(right.position, "z", right.position.z + 240, 2, function(){
+						game.tweens.splice(game.tweens.indexOf(this), 1);
+						delete game.cameraTarget; 
+						const door = game.doors[this.onAction.index];
+						const left = door.doors[0];
+						const right = door.doors[1];
+						const leftProxy = door.proxy[0];
+						const rightProxy = door.proxy[1];
+						leftProxy.position = left.position.clone();
+						rightProxy.position = right.position.clone();
+					}))
+					break;
+                case 'collect':
+                    this.activeCamera = this.player.cameras.collect;
+                    this.collect[this.onAction.index].visible = false;
+                    if (this.collected==undefined) this.collected = [];
+                    this.collected.push(this.onAction.index);
+                    document.getElementById("briefcase").children[0].children[0].children[this.onAction.index].children[0].src = this.onAction.src;
+                    
+                    break;
+			}
+		}
 	}
 	
 	switchCamera(fade=0.05){
@@ -94,12 +184,17 @@ class Game{
 	initSfx(){
 		this.sfx = {};
 		this.sfx.context = new (window.AudioContext || window.webkitAudioContext)();
-		this.sfx.gliss = new SFX({
-			context: this.sfx.context,
-			src:{mp3:`${this.assetsPath}sfx/gliss.mp3`, ogg:`${this.assetsPath}sfx/gliss.ogg`},
-			loop: false,
-			volume: 0.3
-		});
+		const list = ['gliss','door','factory','button','fan'];
+		const game = this;
+		list.forEach(function(item){
+			game.sfx[item] = new SFX({
+				context: game.sfx.context,
+				src:{mp3:`${game.assetsPath}sfx/${item}.mp3`, ogg:`${game.assetsPath}sfx/${item}.ogg`},
+				loop: (item=='factory' || item=='fan'),
+				autoplay: (item=='factory'|| item=='fan'),
+				volume: 0.3
+			});	
+		})
 	}
 	
 	set activeCamera(object){
@@ -132,7 +227,7 @@ class Game{
 		light.shadow.camera.far = 3000;
 		this.scene.add( light );
 
-		/*// ground
+		// ground
 		var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
 		mesh.rotation.x = - Math.PI / 2;
 		//mesh.position.y = -100;
@@ -143,7 +238,7 @@ class Game{
 		//grid.position.y = -100;
 		grid.material.opacity = 0.2;
 		grid.material.transparent = true;
-		//this.scene.add( grid );*/
+		//this.scene.add( grid );
 
 		// model
 		const loader = new THREE.FBXLoader();
@@ -152,6 +247,13 @@ class Game{
 		loader.load( `${this.assetsPath}fbx/girl-walk.fbx`, function ( object ) {
 
 			object.mixer = new THREE.AnimationMixer( object );
+			object.mixer.addEventListener('finished', function(e){
+				game.action = 'look-around';
+                if (game.player.cameras.active == game.player.cameras.collect){
+                    game.activeCamera = game.player.cameras.back;
+                    game.toggleBriefcase();
+                }
+			})
 			object.castShadow = true;
 			
 			game.player.mixer = object.mixer;
@@ -177,7 +279,7 @@ class Game{
 			
 			game.createCameras();
 			game.loadEnvironment(loader);
-		} );
+		}, null, this.onError );
 		
 		this.renderer = new THREE.WebGLRenderer( { antialias: true } );
 		this.renderer.setPixelRatio( window.devicePixelRatio );
@@ -194,32 +296,83 @@ class Game{
 			this.stats = new Stats();
 			this.container.appendChild( this.stats.dom );
 		}
+		
+		this.initSfx();
 	}
 
+    loadUSB(loader){
+		const game = this;
+		
+		loader.load( `${this.assetsPath}fbx/usb.fbx`, function ( object ) {
+			game.scene.add(object);
+			
+            const scale = 0.2;
+			object.scale.set(scale, scale, scale);
+			object.name = "usb";
+            object.position.set(-416, 0.8, -472);
+            object.castShadow = true;
+			
+            game.collect.push(object);
+			
+			object.traverse( function ( child ) {
+				if ( child.isMesh ) {
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+				}
+			} );
+			
+			game.loadNextAnim(loader);
+		}, null, this.onError );
+	}
+    
 	loadEnvironment(loader){
 		const game = this;
 		
 		loader.load( `${this.assetsPath}fbx/environment.fbx`, function ( object ) {
 			game.scene.add(object);
+			game.doors = [];
+			game.fans = [];
 			
 			object.receiveShadow = true;
 			object.scale.set(0.8, 0.8, 0.8);
 			object.name = "Environment";
-					
+			let door = { trigger:null, proxy:[], doors:[]};
+			
 			object.traverse( function ( child ) {
 				if ( child.isMesh ) {
 					if (child.name.includes('main')){
 						child.castShadow = true;
 						child.receiveShadow = true;
-					}else if (child.name.includes('proxy')){
+					}else if (child.name.includes('mentproxy')){
 						child.material.visible = false;
+						game.environmentProxy = child;
+					}else if (child.name.includes('door-proxy')){
+						child.material.visible = false;
+						door.proxy.push(child);
+						checkDoor();
+ 					}else if (child.name.includes('door')){
+						door.doors.push(child);
+						checkDoor()
+					}else if (child.name.includes('fan')){
+						game.fans.push(child);
 					}
-							
+				}else{
+					if (child.name.includes('Door-null')){
+						door.trigger = child;
+						checkDoor();
+					}
+				}
+				
+				function checkDoor(){
+					if (door.trigger!==null && door.proxy.length==2 && door.doors.length==2){
+						game.doors.push(Object.assign({}, door));
+						door = { trigger:null, proxy:[], doors:[]};
+					}
 				}
 			} );
 			
-			game.loadNextAnim(loader);
-		} );
+			game.loadUSB(loader);
+		}, null, this.onError );
 	}
 	
 	createDummyEnvironment(){
@@ -252,11 +405,11 @@ class Game{
 		}
 		
 		if (forward>0){
-			if (this.player.action!='walk') this.action = 'walk';
+			if (this.player.action!='walk'&& this.player.action!='run') this.action = 'walk';
 		}else if (forward<-0.2){
 			if (this.player.action!='walk') this.action = 'walk';
 		}else{
-			if (this.player.action=="walk") this.action = 'look-around';
+			if (this.player.action=="walk" || this.player.action=='run') this.action = 'look-around';
 		}
 		
 	}
@@ -292,11 +445,15 @@ class Game{
 		const game = this;
 		loader.load( `${this.assetsPath}fbx/${anim}.fbx`, function( object ){
 			game.player[anim] = object.animations[0];
+			if (anim=='push-button'){
+				game.player[anim].loop = false;
+			}
 			if (game.anims.length>0){
 				game.loadNextAnim(loader);
 			}else{
 				delete game.anims;
 				game.action = "look-around";
+				game.initPlayerPosition();
 				game.mode = game.modes.ACTIVE;
 				const overlay = document.getElementById("overlay");
     			overlay.classList.add("fade-in");
@@ -304,7 +461,22 @@ class Game{
 					evt.target.style.display = 'none';
 				}, false);
 			}
-		});	
+		}, null, this.onError);	
+	}
+	
+	initPlayerPosition(){
+		//cast down
+		const dir = new THREE.Vector3(0,-1,0);
+		const pos = this.player.object.position.clone();
+		pos.y += 200;
+		const raycaster = new THREE.Raycaster(pos, dir);
+		const gravity = 30;
+		const box = this.environmentProxy;
+		
+		const intersect = raycaster.intersectObject(box);
+		if (intersect.length>0){
+			this.player.object.position.y = pos.y - intersect[0].distance;
+		}
 	}
 	
 	getMousePosition(clientX, clientY){
@@ -357,13 +529,17 @@ class Game{
 	}
 
 	set action(name){
+		if (this.player.action==name) return;
 		const anim = this.player[name];
 		const action = this.player.mixer.clipAction( anim,  this.player.root );
 		this.player.mixer.stopAllAction();
 		this.player.action = name;
 		action.timeScale = (name=='walk' && this.player.move!=undefined && this.player.move.forward<0) ? -0.3 : 1;
+        action.time = 0;
 		action.fadeIn(0.5);	
+		if (name=='push-button' || name=='gather-objects') action.loop = THREE.LoopOnce;
 		action.play();
+		this.player.actionTime = Date.now();
 	}
 	
 	movePlayer(dt){
@@ -385,7 +561,8 @@ class Game{
 		
 		if (!blocked){
 			if (this.player.move.forward>0){
-				this.player.object.translateZ(dt*100);
+				const speed = (this.player.action=='run') ? 200 : 100;
+				this.player.object.translateZ(dt*speed);
 			}else{
 				this.player.object.translateZ(-dt*30);
 			}
@@ -447,10 +624,18 @@ class Game{
 		
 		requestAnimationFrame( function(){ game.animate(); } );
 		
+		if (this.tweens.length>0){
+			this.tweens.forEach(function(tween){ tween.update(dt); });	
+		}
+		
 		if (this.player.mixer!=undefined && this.mode==this.modes.ACTIVE){
 			this.player.mixer.update(dt);
 		}
 		
+		if (this.player.action=='walk'){
+			const elapsedTime = Date.now() - this.player.actionTime;
+			if (elapsedTime>1000 && this.player.move.forward>0) this.action = 'run';
+		}
 		if (this.player.move!=undefined){
 			if (this.player.move.forward!=0) this.movePlayer(dt);
 			this.player.object.rotateY(this.player.move.turn*dt);
@@ -458,16 +643,64 @@ class Game{
 		
 		if (this.player.cameras!=undefined && this.player.cameras.active!=undefined){
 			this.camera.position.lerp(this.player.cameras.active.getWorldPosition(new THREE.Vector3()), this.cameraFade);
-			const pos = this.player.object.position.clone();
-			pos.y += 60;
+			let pos;
+			if (this.cameraTarget!=undefined){
+				this.camera.position.copy(this.cameraTarget.position);
+				pos = this.cameraTarget.target;
+			}else{
+				pos = this.player.object.position.clone();
+				pos.y += 60;
+			}
 			this.camera.lookAt(pos);
 		}
 		
+		this.actionBtn.style = 'display:none;';
+		let trigger = false;
+		
+		if (this.doors !== undefined){
+			this.doors.forEach(function(door){
+				if (game.player.object.position.distanceTo(door.trigger.position)<100){
+					game.actionBtn.style = 'display:block;';
+					game.onAction = { action:'push-button', mode:'open-doors', index:0 };
+					trigger = true;
+				}
+			});
+		}
+        
+        if (this.collect !== undefined && !trigger){
+            this.collect.forEach(function(object){
+				if (object.visible && game.player.object.position.distanceTo(object.position)<100){
+					game.actionBtn.style = 'display:block;';
+					game.onAction = { action:'gather-objects', mode:'collect', index:0, src:"usb.jpg" };
+					trigger = true;
+				}
+			});
+        }
+		
+		if (!trigger) delete this.onAction;
+		
+		if (this.fans !== undefined){
+            let vol = 0;
+            this.fans.forEach(function(fan){
+                const dist = fan.position.distanceTo(game.player.object.position);
+                const tmpVol = 1 - dist/1000;
+                if (tmpVol>vol) vol = tmpVol;
+                fan.rotateZ(dt); 
+            });
+            this.sfx.fan.volume = vol;
+        }
+	
 		this.renderer.render( this.scene, this.camera );
 
 		if (this.stats!=undefined) this.stats.update();
 
 	}
+	
+	onError(error){
+		const msg = 
+		console.error(JSON.stringify(error));
+		console.error(error.message);
+	} 
 }
 
 class Easing{
@@ -806,7 +1039,7 @@ class SFX{
 class JoyStick{
 	constructor(options){
 		const circle = document.createElement("div");
-		circle.style.cssText = "position:absolute; bottom:35px; width:80px; height:80px; background:rgba(126, 126, 126, 0.5); border:#444 solid medium; border-radius:50%; left:50%; transform:translateX(-50%);";
+		circle.style.cssText = "position:absolute; bottom:35px; width:80px; height:80px; background:rgba(126, 126, 126, 0.5); border:#fff solid medium; border-radius:50%; left:50%; transform:translateX(-50%);";
 		const thumb = document.createElement("div");
 		thumb.style.cssText = "position: absolute; left: 20px; top: 20px; width: 40px; height: 40px; border-radius: 50%; background: #fff;";
 		circle.appendChild(thumb);
